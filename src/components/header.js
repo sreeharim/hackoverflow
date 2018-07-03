fcb48f42af40dc58362f301505176feac71aaee6
Sreehari M <sreeharim1992@gmail.com>
2018-07-03 22:34:41 +0000

    making logo to redirect to home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { headerStyle, logoStyle } = styles;
   return (
     <div style={headerStyle}>
-      <span style={logoStyle}>
-        <img src="../images/logo_small.png" />
-      </span>
+      <Link to="/">
+        <span style={logoStyle}>
+          <img src="../images/logo_small.png" />
+        </span>
+      </Link>
     </div>
   );
 };
